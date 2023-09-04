c8a3d7a8f5dd8fcd0fe0ea0ca9061c63fbc91af4
crazydevlegend <crazy010323@gmail.com>
2023-09-04 14:29:45 -0400

    feat: plugin functionality

--- pages/api/plugins/index.ts
+++ pages/api/plugins/index.ts
@@ -3,8 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import googleSearch from './google-search';
 import weather from './open-weather';
 import news from './world-news';
+import wolframAlpha from './wolfram-alpha';
 
-export const plugins = [weather, news, googleSearch];
+export const plugins = [weather, news, googleSearch, wolframAlpha];
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.json(
